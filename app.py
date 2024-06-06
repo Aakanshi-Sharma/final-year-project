2fb0f6ad24174384849add992a23fbc50a73fb6d
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2024-06-06 22:41:39 +0530

    project UI completed

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from feature import FeatureExtraction
+
+model = pd.read_pickle(open("model.pkl", "rb"))
 
 
-st.title("sdffdv")
+def prediction(url):
+    obj = FeatureExtraction(url)
+    x = np.array(obj.getFeaturesList()).reshape(1, 30)
+    result = model.predict(x)[0]
+    y_pro_phishing = model.predict_proba(x)[0, 0]
+    y_pro_non_phishing = model.predict_proba(x)[0, 1]
+    # 1 is safe
+    # -1 is unsafe
+    print("y_pro_phishing", y_pro_phishing)
+    print("y_pro_non_phishing", y_pro_non_phishing)
+    if result == 1:
+        return "Safe" + " It is {0:.2f} % safe to go ".format(y_pro_non_phishing * 100)
+    elif result == -1:
+        return "Unsafe" + " It is {0:.2f} % unsafe".format(y_pro_phishing * 100)
+    else:
+        return "Something went wrong!"
 
-model=pd.read_pickle(open("model.pkl", "rb"))
 
-st.header(model.predict([[0,1,1,1,1,1,-1,0,1,-1,1,1,-1,1,0,-1,-1,1,1,0,1,1,1,1,-1,-1,0,-1,1,1]]))
+st.title("Phishing URL Detection")
+resultant_url = st.text_input("Enter the URL")
+if st.button("Click here..", type="primary"):
+    st.subheader(prediction(resultant_url))
