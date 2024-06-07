edcc48f4c1a3c38783506468901b0309845facf9
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2024-06-07 23:34:57 +0530

    final check in

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from feature import FeatureExtraction
+from urllib.parse import urlparse
 
 model = pd.read_pickle(open("model.pkl", "rb"))
 
 
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
 def prediction(url):
+    isCHeckedUrl = is_valid_url(url)
+    if not isCHeckedUrl:
+        return -2, 0, 0
     obj = FeatureExtraction(url)
     x = np.array(obj.getFeaturesList()).reshape(1, 30)
     result = model.predict(x)[0]
@@ -16,15 +28,19 @@ def prediction(url):
     # -1 is unsafe
     print("y_pro_phishing", y_pro_phishing)
     print("y_pro_non_phishing", y_pro_non_phishing)
-    if result == 1:
-        return "Safe" + " It is {0:.2f} % safe to go ".format(y_pro_non_phishing * 100)
-    elif result == -1:
-        return "Unsafe" + " It is {0:.2f} % unsafe".format(y_pro_phishing * 100)
-    else:
-        return "Something went wrong!"
+    return result, y_pro_phishing, y_pro_non_phishing
 
 
 st.title("Phishing URL Detection")
 resultant_url = st.text_input("Enter the URL")
 if st.button("Click here..", type="primary"):
-    st.subheader(prediction(resultant_url))
+    with st.spinner('Wait for it...'):
+        res = prediction(resultant_url)
+        if res[0] == 1:
+            st.success("Safe ✅" + " It is {0:.2f} % safe to go ".format(res[2] * 100))
+        elif res[0] == -1:
+            st.error("Unsafe 🚨" + " It is {0:.2f} % unsafe".format(res[1] * 100))
+        elif res[0] == -2:
+            st.warning("Invalid URL ⚠️")
+        else:
+            st.warning("Something went wrong!")
