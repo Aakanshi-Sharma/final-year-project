5a9e1d9af19714067a1cdf0f51aedc9c9c72b328
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2024-06-09 22:16:17 +0530

    changes

--- app.py
+++ app.py
@@ -3,9 +3,23 @@ import pandas as pd
 import numpy as np
 from feature import FeatureExtraction
 from urllib.parse import urlparse
+import google.generativeai as genai
+
+# -------------GLOBAL VARIABLES--------
+
+with open("key.txt", "r") as f:
+    GOOGLE_API_KEY = f.read()
+
+genai.configure(api_key=GOOGLE_API_KEY)
 
 model = pd.read_pickle(open("model.pkl", "rb"))
 
+
+def get_response_genai(url):
+    model1 = genai.GenerativeModel('gemini-pro')
+    response = model1.generate_content(f"Predict whether the url {url} is safe or unsafe and give the percentage and reason in 1 line")
+    print(response.text)
+    return response.text
 
 def is_valid_url(url):
     try:
@@ -14,6 +28,10 @@ def is_valid_url(url):
     except ValueError:
         return False
 
+
+def predict(url):
+    obj = get_response_genai(url)
+    return obj
 
 def prediction(url):
     isCHeckedUrl = is_valid_url(url)
@@ -33,14 +51,16 @@ def prediction(url):
 
 st.title("Phishing URL Detection")
 resultant_url = st.text_input("Enter the URL")
-if st.button("Click here..", type="primary"):
-    with st.spinner('Wait for it...'):
-        res = prediction(resultant_url)
-        if res[0] == 1:
-            st.success("Safe ✅" + " It is {0:.2f} % safe to go ".format(res[2] * 100))
-        elif res[0] == -1:
-            st.error("Unsafe 🚨" + " It is {0:.2f} % unsafe".format(res[1] * 100))
-        elif res[0] == -2:
-            st.warning("Invalid URL ⚠️")
-        else:
-            st.warning("Something went wrong!")
+if st.button("Click here...", type="primary"):
+    st.success(predict(resultant_url))
+# if st.button("Click here..", type="primary"):
+#     with st.spinner('Wait for it...'):
+#         res = prediction(resultant_url)
+#         if res[0] == 1:
+#             st.success("Safe ✅" + " It is {0:.2f} % safe to go ".format(res[2] * 100))
+#         elif res[0] == -1:
+#             st.error("Unsafe 🚨" + " It is {0:.2f} % unsafe".format(res[1] * 100))
+#         elif res[0] == -2:
+#             st.warning("Invalid URL ⚠️")
+#         else:
+#             st.warning("Something went wrong!")
